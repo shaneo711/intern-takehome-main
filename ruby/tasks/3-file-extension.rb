6db0a804c365bfc86c281d16c4b35364c555124c
Shane Garland <shane.garland1@gmail.com>
2022-03-03 10:22:00 +1100

    completed file extension problem

--- ruby/tasks/3-file-extension.rb
+++ ruby/tasks/3-file-extension.rb
@@ -6,5 +6,7 @@
  # @returns {String} The file extension (with no period), or false if there is none.
 #
 def get_file_extension(filename)
+  return filename.split('.')[1] if filename.include? '.'
 
+  false
 end
