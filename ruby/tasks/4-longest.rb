1844d21461d0b7d9505123c1eb29b627a5f624d3
Shane Garland <shane.garland1@gmail.com>
2022-03-03 10:33:24 +1100

    completed longest challenge

--- ruby/tasks/4-longest.rb
+++ ruby/tasks/4-longest.rb
@@ -6,5 +6,10 @@
  # @returns {String} The longest string from within the input array.
 #
 def longest_string(strings)
-
+  # Reduce input to a single array.
+  strings.flatten!
+  # Remove elements that are not of the array type.
+  strings.select! { |element| element.is_a? String }
+  # Return the strings with the longest length.
+  strings.max_by(&:length)
 end
