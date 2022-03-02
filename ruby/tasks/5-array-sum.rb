eb856598ef7a60b78eb8bdb0c61f0408f5e5e0ab
Shane Garland <shane.garland1@gmail.com>
2022-03-03 10:36:55 +1100

    completed array_sum challenge

--- ruby/tasks/5-array-sum.rb
+++ ruby/tasks/5-array-sum.rb
@@ -6,6 +6,10 @@
  # @returns {Number} The sum of all integers contained in the input, at any level.
 #
 def array_sum(start_of_tree)
-
+  # Reduce input to a single array.
+  start_of_tree.flatten!
+  # Remove elements that are not of Integer type.
+  start_of_tree.select! { |element| element.is_a? Integer }
+  # Return the sum of the integers.
+  start_of_tree.sum
 end
-
